14191cd9ce1ec0656a90e021370da20ff8ecf2bd
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-18 22:17:11 +0100

    implementar cerrar sesión

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +9,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class Header {
 
+  constructor(private router: Router){}
+
+  cerrarSesion(){
+    localStorage.removeItem('usuarioTwitter');
+    this.router.navigate(['/login']);
+  }
 }
