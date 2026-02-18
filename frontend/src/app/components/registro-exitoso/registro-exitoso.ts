0fb4a5ead45bd527f28f973b21147b67d2470b07
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-18 23:59:21 +0100

    add componente de registro exitoso

---
+++ frontend/src/app/components/registro-exitoso/registro-exitoso.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-exitoso',
+  imports: [],
+  templateUrl: './registro-exitoso.html',
+  styleUrl: './registro-exitoso.css',
+})
+export class RegistroExitoso {
+
+}
